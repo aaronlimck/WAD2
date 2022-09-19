f46686bbd25c2170e6b756c8ce96b5708c455fd0
Herman Tan <herman.tan.2021@scis.smu.edu.sg>
2022-09-19 19:12:34 +0800

    added Event Cards

--- src/components/Events/BrowseEvents.vue
+++ src/components/Events/BrowseEvents.vue
@@ -1,20 +1,63 @@
 <template>
   <the-search-bar />
   <section>
-    <div class="container mx-auto">
-      <h1>This is Event Page</h1>
+    <div class="container mx-auto my-10">
+      <div class="grid grid-cols-3 gap-0 gap-y-4 ">
+        <event-card/>
+        <event-card/>
+        <event-card/>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 import TheSearchBar from "./TheSearchBar.vue";
+import EventCard from "./EventCard.vue";
 
 export default {
-  components: { TheSearchBar },
+  components: { TheSearchBar , EventCard},
   data(){
     return{
-    }
+      events: [
+                  {
+                    "eventId": "eventId1",
+                    "eventName": "Event 1",
+                    "eventDescription": "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                    "eventDateTime": "2022-09-02T18:25:43.511Z",
+                    "eventCreatedBy": "SMU Club",
+                    "eventTags": ["tag1", "tag2", "tag3"],
+                    "eventAttendees": ["studentId1", "studentId2", "studentId3", "studentId4"]
+                  },
+                  {
+                    "eventId": "eventId2",
+                    "eventName": "Event 2",
+                    "eventDescription": "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                    "eventDateTime": "2022-09-02T18:25:43.511Z",
+                    "eventCreatedBy": "SMU Club",
+                    "eventTags": ["tag2", "tag4", "tag1"],
+                    "eventAttendees": ["studentId1", "studentId3", "studentId5"]
+                  },
+                  {
+                    "eventId": "eventId3",
+                    "eventName": "Event 3",
+                    "eventDescription": "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                    "eventDateTime": "2022-09-02T18:25:43.511Z",
+                    "eventCreatedBy": "SMU",
+                    "eventTags": ["tag2", "tag4", "tag1"],
+                    "eventAttendees": ["studentId1", "studentId3", "studentId4", "studentId5"]
+                  },
+                  {
+                    "eventId": "eventId4",
+                    "eventName": "Event 4",
+                    "eventDescription": "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                    "eventDateTime": "2022-09-02T18:25:43.511Z",
+                    "eventCreatedBy": "SMU",
+                    "eventTags": ["tag2", "tag4", "tag2"],
+                    "eventAttendees": ["studentId1", "studentId3", "studentId4", "studentId5"]
+                  }
+                ]
+        }
   }
 };
 </script>
