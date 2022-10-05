32183bab67017dbbd33082ab7a1a427a9a3b746d
Aaron Lim CK <aaronlimckdeveloper@gmail.com>
2022-10-06 02:58:07 +0800

    create components in the signup page

--- src/App.vue
+++ src/App.vue
@@ -8,53 +8,69 @@
 </template>
 
 <script>
-
-  export default{
-    data(){
-      return{
-        events: [
-                  {
-                    "eventId": "eventId1",
-                    "eventName": "Event 1",
-                    "eventDescription": "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                    "eventDateTime": "2022-09-02T18:25:43.511Z",
-                    "eventCreatedBy": "SMU Club",
-                    "eventTags": ["tag1", "tag2", "tag3"],
-                    "eventAttendees": ["studentId1", "studentId2", "studentId3", "studentId4"]
-                  },
-                  {
-                    "eventId": "eventId2",
-                    "eventName": "Event 2",
-                    "eventDescription": "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                    "eventDateTime": "2022-09-02T18:25:43.511Z",
-                    "eventCreatedBy": "SMU Club",
-                    "eventTags": ["tag2", "tag4", "tag1"],
-                    "eventAttendees": ["studentId1", "studentId3", "studentId5"]
-                  },
-                  {
-                    "eventId": "eventId3",
-                    "eventName": "Event 3",
-                    "eventDescription": "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                    "eventDateTime": "2022-09-02T18:25:43.511Z",
-                    "eventCreatedBy": "SMU",
-                    "eventTags": ["tag2", "tag4", "tag1"],
-                    "eventAttendees": ["studentId1", "studentId3", "studentId4", "studentId5"]
-                  },
-                  {
-                    "eventId": "eventId4",
-                    "eventName": "Event 4",
-                    "eventDescription": "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                    "eventDateTime": "2022-09-02T18:25:43.511Z",
-                    "eventCreatedBy": "SMU",
-                    "eventTags": ["tag2", "tag4", "tag2"],
-                    "eventAttendees": ["studentId1", "studentId3", "studentId4", "studentId5"]
-                  }
-                ],
-            }
-          },
-          
-  }
-
+export default {
+  data() {
+    return {
+      events: [
+        {
+          eventId: "eventId1",
+          eventName: "Event 1",
+          eventDescription:
+            "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+          eventDateTime: "2022-09-02T18:25:43.511Z",
+          eventCreatedBy: "SMU Club",
+          eventTags: ["tag1", "tag2", "tag3"],
+          eventAttendees: [
+            "studentId1",
+            "studentId2",
+            "studentId3",
+            "studentId4",
+          ],
+        },
+        {
+          eventId: "eventId2",
+          eventName: "Event 2",
+          eventDescription:
+            "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+          eventDateTime: "2022-09-02T18:25:43.511Z",
+          eventCreatedBy: "SMU Club",
+          eventTags: ["tag2", "tag4", "tag1"],
+          eventAttendees: ["studentId1", "studentId3", "studentId5"],
+        },
+        {
+          eventId: "eventId3",
+          eventName: "Event 3",
+          eventDescription:
+            "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+          eventDateTime: "2022-09-02T18:25:43.511Z",
+          eventCreatedBy: "SMU",
+          eventTags: ["tag2", "tag4", "tag1"],
+          eventAttendees: [
+            "studentId1",
+            "studentId3",
+            "studentId4",
+            "studentId5",
+          ],
+        },
+        {
+          eventId: "eventId4",
+          eventName: "Event 4",
+          eventDescription:
+            "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+          eventDateTime: "2022-09-02T18:25:43.511Z",
+          eventCreatedBy: "SMU",
+          eventTags: ["tag2", "tag4", "tag2"],
+          eventAttendees: [
+            "studentId1",
+            "studentId3",
+            "studentId4",
+            "studentId5",
+          ],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
@@ -66,7 +82,6 @@
 
 html {
   font-family: "Roboto Flex", sans-serif;
-  font-weight: 400;
 }
 
 body {
