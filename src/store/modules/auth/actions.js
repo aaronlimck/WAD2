9032b9c2b32e0e7c4317fc472a15421339de9be2
Aaron Lim CK <aaronlimckdeveloper@gmail.com>
2022-10-06 12:31:33 +0800

    create vuex store and updated signup page with the base structure

---
+++ src/store/modules/auth/actions.js
@@ -0,0 +1,29 @@
+export default {
+  async signup(context, payload) {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAwfv0cPv34-F3iZG0VcyoPoH1ThhC3fUU",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || "Failed to authenticate. Try again later"
+      );
+      throw error;
+    }
+    console.log(responseData);
+    context.commit("setUser", {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
