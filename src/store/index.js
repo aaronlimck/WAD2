c45769dc9960a096db7a3e486477f58fc344b6c5
Aaron Lim CK <aaronlimckdeveloper@gmail.com>
2022-10-19 13:50:32 +0800

    update vuex store with modules path

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 
 import authModule from "./modules/auth/index.js";
+import userModule from "./modules/user/index.js";
 
 const store = createStore({
   modules: {
     auth: authModule,
+    user: userModule,
   },
 });
 
