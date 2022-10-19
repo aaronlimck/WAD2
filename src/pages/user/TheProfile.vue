ec9edab337f2e89bb53ec27b34bba4c4e704b3f5
Aaron Lim CK <aaronlimckdeveloper@gmail.com>
2022-10-19 13:51:06 +0800

    created new page paths

--- src/pages/user/TheProfile.vue
+++ src/pages/user/TheProfile.vue
@@ -1,89 +1,144 @@
-
 <template>
   <section>
-    <div class="container mx-auto">
-    </div>
+    <div class="container mx-auto"></div>
   </section>
-    <TabsWrapper>
-      <Tab title="Profile"> 
-       <form>
-          
-          <img src="../../images/avatar.png" alt="Avatar" class="avatar" 
-          style = "vertical-align: middle;
-          width: 50px;
-          height: 50px;
-          border-radius: 50%;
-          float:left;
-          
-          margin-right:2px;">
+  <TabsWrapper>
+    <Tab title="Profile">
+      <form @submit.prevent="submitForm">
+        <img
+          src="../../images/avatar.png"
+          alt="Avatar"
+          class="avatar"
+          style="
+            vertical-align: middle;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            float: left;
+            margin-right: 2px;
+          "
+        />
 
-          <button class="py-1 px-1.5" id = "change_avatar" style="float:left; margin-right:9px; width:120px;">Change Avatar</button>
-          <button class="py-1 px-3" style="float:left; width:120px">Delete</button>
-          
-          <br><br><br>
-          Name:
-          <br>
-            <input type="text" 
-                  name="user_name"
-                  size="20"
-                  value= "Dehou"  
-                  style="border:1px solid black;">
-          <br><br>        
-          Email:        
-          <br>
-            <input type="text" 
-                  name="user_email"
-                  size="20"
-                  value= "student.2021@scis.smu.edu.sg"  
-                  style="border:1px solid black;">
+        <button
+          class="py-1 px-1.5"
+          id="change_avatar"
+          style="float: left; margin-right: 9px; width: 120px"
+        >
+          Change Avatar
+        </button>
+        <button class="py-1 px-3" style="float: left; width: 120px">
+          Delete
+        </button>
 
-          <br><br>        
-          Phone Number:        
-          <br>
-            <input type="text" 
-                  name="user_phone_number"
-                  size="20"
-                  value= "12345678"  
-                  style="border:1px solid black;">
-          
-          <br><br>        
-          Password:        
-          <br>
-            <button class="py-1 px-3" id = "change_password">Change Password</button>
-            
-          <br><br>
-            <button class="py-1 px-3">Save Profile</button>
-        </form>
-      </Tab>
+        <br /><br /><br />
+        <div class="form-control">
+          <label for="userName">Name</label>
+          <input
+            id="userName"
+            type="text"
+            name="userName"
+            :value="this.$store.getters.userName"
+          />
+        </div>
+        <div class="form-control">
+          <label for="userEmail">Email</label>
+          <input
+            id="userEmail"
+            type="text"
+            name="userEmail"
+            :value="this.$store.getters.userEmail"
+          />
+        </div>
 
-      <Tab title="Registered Events"> 
-        <event-card></event-card>
-        
-      </Tab>
-    </TabsWrapper>
-    
+        <div class="form-control">
+          <label for="userPhone">Phone</label>
+          <input
+            id="userPhone"
+            type="text"
+            name="userPhone"
+            :value="this.$store.getters.userPhone"
+          />
+        </div>
+
+        Password:
+        <br />
+        <button class="py-1 px-3" id="change_password" @click="changePassword">
+          Change Password
+        </button>
+
+        <br /><br />
+        <button class="py-1 px-3">Save Profile</button>
+      </form>
+    </Tab>
+
+    <Tab title="Registered Events">
+      <event-card></event-card>
+    </Tab>
+  </TabsWrapper>
 </template>
 
 <style scoped>
-  input[type=text]{
-    width:280px;
-  }
+form {
+  max-width: 520px;
+}
 
-  button{
-    border: 1px black solid;
-    border-radius:5px;
-  }
+form {
+  max-width: 520px;
+}
 
-  #change_password{
-    background-color:#f56a01;
-    color:white;
-  }
+.form-control {
+  margin: 0.8rem 0;
+}
 
+label {
+  font-weight: 400;
+  color: #787878;
+}
+
+input {
+  display: block;
+  width: 100%;
+  font-weight: 400;
+  border: 1px solid #eee;
+  border-radius: 5px;
+  padding: 0.5rem 1rem;
+}
+
+button {
+  border: 1px black solid;
+  border-radius: 5px;
+}
+
+#change_password {
+  background-color: #f56a01;
+  color: white;
+}
 </style>
 
+<script>
+import Tab from "../../components/UI/TheTab.vue";
+import TabsWrapper from "../../components/UI/TabsWrapper.vue";
 
-<script setup>
-  import Tab from '../../components/UI/TheTab.vue'
-  import TabsWrapper from '../../components/UI/TabsWrapper.vue'
-
+export default {
+  components: { Tab, TabsWrapper },
+  async mounted() {
+    try {
+      await this.$store.dispatch("loadUserData");
+    } catch (err) {
+      //this.error = err.message || "Failed to authenticate, try later";
+    }
+  },
+  methods: {
+    // async changePassword(){
+    //   const token = localStorage.get('token')
+    //   try{
+    //     await this.$store.dispatch("changePassword", {
+    //       idToken: token,
+    //       password:
+    //     })
+    //   }catch(err){
+    //   }
+    // }
+  },
+};
 </script>
