d4592872e1d6061e4b0c68918ece207f8a64ccd0
Aaron Lim CK <aaronlimckdeveloper@gmail.com>
2022-10-22 03:16:49 +0800

    update auth styling and validation

--- src/pages/auth/SignIn.vue
+++ src/pages/auth/SignIn.vue
@@ -1,110 +1,222 @@
 <template>
-  <base-alert
-    v-if="showDialog"
-    classes="text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
-  >
-    <template #default>
-      <p>{{ error }}</p>
-    </template>
-    <template #actions>
-      <button
-        @click="dismissDialog"
-        type="button"
-        class="ml-auto -mx-1.5 -my-1.5 bg-red-100 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex h-8 w-8 dark:bg-red-200 dark:text-red-600 dark:hover:bg-red-300"
-        data-dismiss-target="#alert-2"
-        aria-label="Close"
-      >
-        <span class="sr-only">Close</span>
-        <svg
-          class="w-5 h-5"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
-      </button>
-    </template>
-  </base-alert>
-  <div>
-    <form @submit.prevent="submitForm" class="container p-4 mx-auto">
+  <div id="content">
+    <form @submit.prevent="submitForm" class="container p-4 mx-auto" novalidate>
+      <h1 class="text-2xl md:text-3xl mb-1 md:mb-2 font-medium">Login</h1>
+      <p class="text-base mb-2 md:mb-2">Hi, Welcome back 👋</p>
+
+      <div class="login-with-google-btn mt-3">
+        <img
+          class="google-icon"
+          src="https://cdn-icons-png.flaticon.com/512/300/300221.png"
+          alt="google-icon"
+        />
+        <p class="text-sm">Sign in with Google</p>
+      </div>
+
+      <p class="text-divider"><span>Or</span></p>
+
       <div class="form-control">
         <label for="email">Email</label>
         <input
           type="email"
           id="email"
-          placeholder="Your email address"
+          placeholder="johndoe@email.com"
           v-model.trim="email"
+          @blur="checkUserEmail"
         />
+        <div
+          v-if="error.email != ''"
+          class="flex p-4 my-2 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+          role="alert"
+        >
+          <svg
+            aria-hidden="true"
+            class="flex-shrink-0 inline w-5 h-5 mr-3"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span class="sr-only">Info</span>
+          <div>
+            <span class="font-medium">{{ error.email }}</span>
+          </div>
+        </div>
       </div>
       <div class="form-control">
         <label for="password">Password</label>
-        <input
-          class="w-full"
-          v-if="showPassword"
-          type="text"
-          v-model="password"
-        />
-        <input
-          class="w-full"
-          v-else
-          type="password"
-          id="password"
-          placeholder="••••••••"
-          v-model="password"
-        />
+
+        <div class="relative">
+          <input
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+            v-model="password"
+            @blur="checkUserPassword"
+          />
+          <div
+            @click="switchVisibility"
+            class="absolute right-0 bottom-0 font-medium rounded-lg text-sm px-3 py-2"
+          >
+            <span v-if="showPassword"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6 text-gray-500"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                />
+              </svg>
+            </span>
+            <span v-if="showPassword != true"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6 text-gray-500"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
+
+        <div
+          v-if="error.password != ''"
+          class="flex p-4 my-2 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+          role="alert"
+        >
+          <svg
+            aria-hidden="true"
+            class="flex-shrink-0 inline w-5 h-5 mr-3"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span class="sr-only">Info</span>
+          <div>
+            <span class="font-medium">{{ error.password }}</span>
+          </div>
+        </div>
       </div>
       <div class="flex justify-between">
         <div class="flex flex-row items-center">
           <input
             id="rememberMeCheckbox"
             type="checkbox"
-            value=""
             class="checkbox rounded"
             v-model="rememberMeCheckbox"
+            @change="rememberMe"
           />
-          <label
-            for="rememberMeCheckbox"
-            class="ml-2 text-gray-900 dark:text-gray-300"
-            >Remember Me</label
-          >
+          <label for="rememberMeCheckbox" class="md:ml-2">Remember Me</label>
         </div>
-        <router-link to="/forgot-password">Forget Password?</router-link>
+        <router-link to="/forgot-password" class="text-sm"
+          >Forget Password?</router-link
+        >
       </div>
       <base-button class="w-full py-2.5 my-4">Login</base-button>
       <div class="flex justify-center">
-        <p class="mr-1">Not registered yet?</p>
-        <router-link to="/signup">Create an account</router-link>
+        <p class="mr-1 text-sm">Not registered yet?</p>
+        <router-link to="/signup" class="text-sm font-medium underline"
+          >Create an account</router-link
+        >
       </div>
     </form>
   </div>
 </template>
 
 <script>
-import BaseAlert from "../../components/UI/BaseAlert.vue";
-
 export default {
-  components: { BaseAlert },
   data() {
     return {
       name: "",
-      email: "",
+      email:
+        localStorage.getItem("userEmail") != null
+          ? localStorage.getItem("userEmail")
+          : "",
       password: "",
-      rememberMeCheckbox: false,
+      rememberMeCheckbox:
+        localStorage.getItem("userEmail") != null ? true : false,
       showPassword: false,
-      error: null,
-      showDialog: false,
+      error: {
+        email: "",
+        password: "",
+      },
     };
   },
   methods: {
-    toggleShow() {
-      this.showPassword = !this.showPassword;
+    rememberMe() {
+      if (this.rememberMeCheckbox) {
+        localStorage.setItem("userEmail", this.email);
+      } else {
+        localStorage.removeItem("userEmail");
+      }
     },
-    async submitForm() {
+    switchVisibility() {
+      const passwordField = document.querySelector("#password");
+      if (passwordField.getAttribute("type") === "password") {
+        passwordField.setAttribute("type", "text");
+        this.showPassword = true;
+      } else {
+        passwordField.setAttribute("type", "password");
+        this.showPassword = false;
+      }
+    },
+    checkUserEmail() {
+      if (this.email.length == 0) {
+        this.error.email = "Email field cannot be empty";
+        return false;
+      } else if (!this.email.includes("@")) {
+        this.error.email = "Invalid email address";
+        return false;
+      }
+      this.error.email = "";
+      return true;
+    },
+    checkUserPassword() {
+      if (this.password.length == 0) {
+        this.error.password = "Password field cannot be empty";
+        return false;
+      }
+      this.error.password = "";
+      return true;
+    },
+    submitForm() {
+      this.checkUserEmail();
+      this.checkUserPassword();
+      if (this.error.email == "" && this.error.password == "") {
+        this.login();
+      }
+    },
+    async login() {
       try {
         const loginResult = await this.$store.dispatch("login", {
           email: this.email,
@@ -115,17 +227,30 @@ export default {
           this.$router.replace("/events");
         }
       } catch (err) {
-        //this.error = err.message || "Failed to authenticate, try later";
+        if (err === "EMAIL_NOT_FOUND") {
+          this.error.email =
+            "We couldn't find an account with that email address.";
+          this.error.password = "";
+          this.password = "";
+        }
+        if (err == "INVALID_PASSWORD") {
+          this.error.password =
+            "The password you entered is incorrect. Please try again.";
+          this.error.email = "";
+          this.password = "";
+        }
+        console.log(err);
       }
-    },
-    dismissDialog() {
-      this.showDialog = false;
     },
   },
 };
 </script>
 
 <style scoped>
+#content {
+  padding-top: 50px;
+}
+
 form {
   max-width: 520px;
 }
@@ -139,6 +264,7 @@ form {
 }
 
 label {
+  font-size: 14px;
   font-weight: 400;
   color: #787878;
 }
@@ -155,5 +281,39 @@ input {
 .checkbox {
   width: 15px;
   height: 15px;
+}
+
+.google-icon {
+  width: 20px;
+  height: 20px;
+  margin: 12px;
+}
+
+.login-with-google-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: inherit;
+  border: 1px solid #eee;
+  border-radius: 5px;
+  color: #757575;
+  font-weight: 500;
+  text-align: center;
+  background-color: white;
+}
+
+.text-divider {
+  font-size: 14px;
+  color: #787878;
+  width: 100%;
+  text-align: center;
+  border-bottom: 1px solid #eee;
+  line-height: 0.1em;
+  margin: 30px 0;
+}
+
+.text-divider span {
+  background: #fff;
+  padding: 0 10px;
 }
 </style>
--- src/pages/auth/SignUp.vue
+++ src/pages/auth/SignUp.vue
@@ -1,64 +1,229 @@
 <template>
-  <div>
-    <form @submit.prevent="submitForm" class="container p-4 mx-auto">
+  <div id="content">
+    <form @submit.prevent="submitForm" class="container p-4 mx-auto" novalidate>
+      <h1 class="text-2xl md:text-3xl mb-1 md:mb-2 font-medium">
+        Create an account
+      </h1>
+      <p class="text-base mb-2 md:mb-2">
+        Better way to discover events in SMU 🎉
+      </p>
+
+      <div class="login-with-google-btn mt-3">
+        <img
+          class="google-icon"
+          src="https://cdn-icons-png.flaticon.com/512/300/300221.png"
+          alt="google-icon"
+        />
+        <p class="text-sm">Sign up with Google</p>
+      </div>
+
+      <p class="text-divider"><span>Or</span></p>
+
       <div class="form-control">
         <label for="name">Name</label>
         <input
           type="text"
           id="name"
-          placeholder="First and last name"
+          placeholder="Enter your name"
           v-model.trim="userName"
+          @blur="checkUserName"
         />
+        <div
+          v-if="error.name != ''"
+          class="flex p-4 my-2 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+          role="alert"
+        >
+          <svg
+            aria-hidden="true"
+            class="flex-shrink-0 inline w-5 h-5 mr-3"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span class="sr-only">Info</span>
+          <div>
+            <span class="font-medium">{{ error.name }}</span>
+          </div>
+        </div>
       </div>
       <div class="form-control">
         <label for="email">Email</label>
         <input
           type="email"
           id="email"
-          placeholder="Your email address"
+          placeholder="Enter your email"
           v-model.trim="email"
-        />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input
-          class="w-full"
-          v-if="showPassword"
-          type="text"
-          v-model="password"
-        />
-        <input
-          class="w-full"
-          v-else
-          type="password"
-          id="password"
-          placeholder="••••••••"
-          v-model="password"
+          @blur="checkUserEmail"
         />
         <div
-          class="flex py-2 mb-4 text-sm text-stone-700 rounded-lg"
+          v-if="error.email != ''"
+          class="flex p-4 my-2 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+          role="alert"
+        >
+          <svg
+            aria-hidden="true"
+            class="flex-shrink-0 inline w-5 h-5 mr-3"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span class="sr-only">Info</span>
+          <div>
+            <span class="font-medium">{{ error.email }}</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-control">
+        <label for="password">Password</label>
+        <div class="relative">
+          <input
+            id="password"
+            type="password"
+            placeholder="Create a password"
+            v-model="password"
+            @focus="showPassRequirements"
+            @blur="checkUserPassword"
+          />
+          <div
+            @click="switchVisibility"
+            class="absolute right-0 bottom-0 font-medium rounded-lg text-sm px-3 py-2"
+          >
+            <span v-if="showPassword"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6 text-gray-500"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                />
+              </svg>
+            </span>
+            <span v-if="showPassword != true"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6 text-gray-500"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
+
+        <div
+          v-if="error.password != ''"
+          class="flex p-4 my-2 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+          role="alert"
+        >
+          <svg
+            aria-hidden="true"
+            class="flex-shrink-0 inline w-5 h-5 mr-3"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span class="sr-only">Info</span>
+          <div>
+            <span class="font-medium">{{ error.password }}</span>
+          </div>
+        </div>
+
+        <div
+          v-show="showPasswordRequirements"
+          class="fade-in flex p-3 my-4 text-sm text-stone-700 rounded-lg custom-border"
           role="alert"
         >
           <span class="sr-only">Info</span>
           <div>
             <span class="font-medium">Password must:</span>
             <ul class="mt-1.5 ml-4 text-stone-800 list-disc list-inside">
-              <li>Be a minimum of 8 characters</li>
-              <li>Include at least one lowercase letter (a-z)</li>
-              <li>Include at least one uppercase letter (A-Z)</li>
-              <li>Include at least one number (0-9)</li>
+              <li
+                :style="[password.length < 7 ? 'color: red' : 'color: green']"
+              >
+                Be a minimum of 8 characters
+              </li>
+              <li
+                :style="[
+                  !stringContainsLowercase(password)
+                    ? 'color: red'
+                    : 'color: green',
+                ]"
+              >
+                Include at least one lowercase letter (a-z)
+              </li>
+              <li
+                :style="[
+                  !stringContainsUppercase(password)
+                    ? 'color: red'
+                    : 'color: green',
+                ]"
+              >
+                Include at least one uppercase letter (A-Z)
+              </li>
+              <li
+                :style="[
+                  !stringContainsNumber(password)
+                    ? 'color: red'
+                    : 'color: green',
+                ]"
+              >
+                Include at least one number (0-9)
+              </li>
             </ul>
           </div>
         </div>
       </div>
-      <base-button class="w-full py-2.5 mb-4">Create my account</base-button>
+      <base-button class="w-full py-2.5 mt-2 mb-4"
+        >Create my account</base-button
+      >
       <p class="text-sm text-center text-gray-900">
-        By continuing, you agree to the Terms of use, Privacy Policy, and
-        Community Standards of Bojio
+        By continuing, you agree to the
+        <span class="underline">Terms of use</span>,
+        <span class="underline">Privacy Policy</span>, and
+        <span class="underline">Community Standards</span> of Bojio
       </p>
       <div class="flex justify-center mt-4">
-        <p class="mr-1">Already have an account?</p>
-        <router-link to="/login">Log In</router-link>
+        <p class="mr-1 text-sm">Already have an account?</p>
+        <router-link to="/login" class="text-sm font-medium underline"
+          >Log In</router-link
+        >
       </div>
     </form>
   </div>
@@ -72,15 +237,87 @@ export default {
       email: "",
       password: "",
       showPassword: false,
-      error: null,
+      showPasswordRequirements: false,
+      error: {
+        name: "",
+        email: "",
+        password: "",
+      },
       showDialog: true,
     };
   },
-  methods: {
-    toggleShow() {
-      this.showPassword = !this.showPassword;
+  watch: {
+    password(newValue) {
+      this.password = newValue;
     },
-    async submitForm() {
+  },
+  methods: {
+    switchVisibility() {
+      const passwordField = document.querySelector("#password");
+      if (passwordField.getAttribute("type") === "password") {
+        passwordField.setAttribute("type", "text");
+        this.showPassword = true;
+      } else {
+        passwordField.setAttribute("type", "password");
+        this.showPassword = false;
+      }
+    },
+    showPassRequirements() {
+      this.showPasswordRequirements = true;
+    },
+    stringContainsUppercase(str) {
+      const pattern = new RegExp("^(?=.*[A-Z])");
+      return pattern.test(str);
+    },
+    stringContainsLowercase(str) {
+      const pattern = new RegExp("^(?=.*[a-z])");
+      return pattern.test(str);
+    },
+    stringContainsNumber(str) {
+      const pattern = new RegExp("^(?=.*\\d)");
+      return pattern.test(str);
+    },
+    checkUserName() {
+      if (this.userName.length == 0) {
+        this.error.name = "Name field cannot be empty";
+        return false;
+      }
+      this.error.name = "";
+      return true;
+    },
+    checkUserEmail() {
+      if (this.email.length == 0) {
+        this.error.email = "Email field cannot be empty";
+        return false;
+      } else if (!this.email.includes("@")) {
+        this.error.email = "Invalid email address";
+        return false;
+      }
+      this.error.email = "";
+      return true;
+    },
+    checkUserPassword(str) {
+      const pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)");
+      if (this.password.length == 0) {
+        this.error.password = "Password field cannot be empty";
+        return false;
+      }
+      this.error.password = "";
+      return pattern.test(str) && str.length >= 8;
+    },
+    submitForm() {
+      this.checkUserName();
+      this.checkUserEmail();
+      this.checkUserPassword(this.password);
+      if (
+        this.error.name == "" &&
+        this.error.email == "" &&
+        this.checkUserPassword(this.password)
+      ) {
+        this.signUp();
+      }
+    },
+    async signUp() {
       try {
         const createResultStatus = await this.$store.dispatch("signup", {
           email: this.email,
@@ -97,7 +334,12 @@ export default {
           this.$router.replace("/events");
         }
       } catch (err) {
-        this.error = err.message || "Failed to authenticate, try later";
+        if (err === "EMAIL_EXISTS") {
+          this.error.email = "This email address has already been taken";
+          this.error.password = "";
+          this.password = "";
+        }
+        console.log(err);
       }
     },
     dismissDialog() {
@@ -108,6 +350,10 @@ export default {
 </script>
 
 <style scoped>
+#content {
+  padding-top: 50px;
+}
+
 form {
   max-width: 520px;
 }
@@ -121,6 +367,7 @@ form {
 }
 
 label {
+  font-size: 14px;
   font-weight: 400;
   color: #787878;
 }
@@ -132,5 +379,60 @@ input {
   border: 1px solid #eee;
   border-radius: 5px;
   padding: 0.5rem 1rem;
+}
+
+.fade-in {
+  opacity: 1;
+  animation-name: fadeInOpacity;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in;
+  animation-duration: 0.3s;
+}
+
+@keyframes fadeInOpacity {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.custom-border {
+  border: 1px solid #eee;
+}
+
+.google-icon {
+  width: 20px;
+  height: 20px;
+  margin: 12px;
+}
+
+.login-with-google-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: inherit;
+  border: 1px solid #eee;
+  border-radius: 5px;
+  color: #757575;
+  font-weight: 500;
+  text-align: center;
+  background-color: white;
+}
+
+.text-divider {
+  font-size: 14px;
+  color: #787878;
+  width: 100%;
+  text-align: center;
+  border-bottom: 1px solid #eee;
+  line-height: 0.1em;
+  margin: 30px 0;
+}
+
+.text-divider span {
+  background: #fff;
+  padding: 0 10px;
 }
 </style>
